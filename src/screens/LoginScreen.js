f61e1950988510e7c65d0785fb2586e408d11f7f
Cornel Bicuti <cornel.bicuti@gmail.com>
2017-06-21 23:20:22 +0100

    Login screen updates

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,16 +5,25 @@ import styled from 'styled-components/native';
 
 class LoginScreen extends Component {
 
+    state = { email: '', password: '' }
+
     render() {
 
         return (
             <View style={{ flex:1, marginTop: 50 }}>
                 <FormLabel>Email Address</FormLabel>
-                <FormInput onChangeText={ () => {} }/>
+                <FormInput
+                    value={this.state.email}
+                    onChangeText={ (email) => this.setState({ email }) }
+                />
                 <FormValidationMessage></FormValidationMessage>
 
                 <FormLabel>Password</FormLabel>
-                <FormInput onChangeText={ () => {} }/>
+                <FormInput
+                    value={this.state.password}
+                    secureTextEntry={true}
+                    onChangeText={ (password) => this.setState({ password }) }
+                />
                 <FormValidationMessage></FormValidationMessage>
         
                 <Button
@@ -23,6 +32,7 @@ class LoginScreen extends Component {
                     title="SIGN IN"
                     color="#fff"
                     backgroundColor="#009688"
+                    onPress={ () => console.log(`values are ${this.state.email} and ${this.state.password}`) }
                 />
                 
             </View>
